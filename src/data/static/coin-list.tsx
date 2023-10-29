bd3c9d2d0a07d11f600ca7b364572b5323bc6e5d
Chomtana <Chomtana001@gmail.com>
2023-10-29 23:03:59 +0700

    Update token list

--- src/data/static/coin-list.tsx
+++ src/data/static/coin-list.tsx
@@ -9,44 +9,45 @@ import { Doge } from '@/components/icons/doge';
 export const coinList = [
   {
     icon: <Bitcoin />,
-    code: 'BTC',
-    name: 'Bitcoin',
+    code: 'PYT',
+    name: 'PhayaThai',
     price: 19076.29,
   },
-  {
-    icon: <Ethereum />,
-    code: 'ETH',
-    name: 'Ethereum',
-    price: 1053.28,
-  },
+  // {
+  //   icon: <Ethereum />,
+  //   code: 'ETH',
+  //   name: 'Ethereum',
+  //   price: 1053.28,
+  // },
   {
     icon: <Tether />,
-    code: 'USDT',
-    name: 'Tether USD',
+    code: 'TLR',
+    name: 'Thonglor',
     price: 0.999,
   },
   {
-    icon: <Bnb />,
-    code: 'BNB',
-    name: 'Binance Coin',
-    price: 214.96,
-  },
-  {
-    icon: <Usdc />,
-    code: 'USDC',
-    name: 'USD Coin',
-    price: 1.001,
-  },
-  {
     icon: <Cardano />,
-    code: 'ADA',
-    name: 'Cardano',
+    code: 'LPO',
+    name: 'LatPhrao',
     price: 0.448,
   },
   {
-    icon: <Doge />,
-    code: 'DOGE',
-    name: 'Doge Coin',
-    price: 0.065,
+    icon: <Bnb />,
+    code: 'STN',
+    name: 'Sathon',
+    price: 214.96,
   },
+  // {
+  //   icon: <Usdc />,
+  //   code: 'USDC',
+  //   name: 'USD Coin',
+  //   price: 1.001,
+  // },
+
+  // {
+  //   icon: <Doge />,
+  //   code: 'DOGE',
+  //   name: 'Doge Coin',
+  //   price: 0.065,
+  // },
 ];
