8d49945582b19e0a7e185f91ce2ea5b5234ba910
Dong <dongdooo@hotmail.com>
2023-11-10 13:21:11 +0700

    fix text

--- src/components/ui/chats/overview-chart.tsx
+++ src/components/ui/chats/overview-chart.tsx
@@ -58,10 +58,10 @@ export default function OverviewChart({ chartWrapperClass }: Props) {
       )}
     >
       <h3 className="text-xl font-medium tracking-tighter text-white sm:text-3xl">
-        74.8%
+        35.79 THB/USD
       </h3>
       <p className="mt-2 mb-1 text-xs font-medium text-gray-400 sm:text-sm">
-        Compare to $1,812 last week
+        Last week: 35.43 THB/USD <br />(Band Oracle)
       </p>
       <div className={cn('h-60 w-full', chartWrapperClass)}>
         <ResponsiveContainer width="100%" height="100%">
